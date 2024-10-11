0bbf5d625e622d72110931e692d722c7abb4c80c
lemon <lemon@example.com>
2024-10-12 02:52:38 +0300

    Deploy client build

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('express-async-errors')
+const path = require('path');
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const usersRouter = require('./controllers/users')
@@ -10,9 +11,6 @@ const loginRouter = require('./controllers/login')
 const commentsRouter = require('./controllers/comments')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
-const path = require('path');
-
 
 mongoose.set('strictQuery', false)
 mongoose
@@ -29,14 +27,7 @@ app.use(express.json())
 app.use(middleware.morgan('combined'))
 app.use(middleware.tokenExtractor)
 
-// Используем express для отдачи статических файлов
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Обрабатываем маршруты, которые не начинаются с /api
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
-// app.use(express.static('dist'));
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
