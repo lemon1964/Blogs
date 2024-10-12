769b98fc5d3faf0b294b0dd3b054e7241ac367df
lemon <lemon@example.com>
2024-10-12 06:38:14 +0300

    Changes

--- tests/playwright/front-page.spec.js
+++ tests/playwright/front-page.spec.js
@@ -4,7 +4,8 @@ describe('Blogs', () => {
   test('front page can be opened after login and displays blogs app', async ({
     page
   }) => {
-    await page.goto('http://localhost:5173/')
+    await page.goto('')
+    // await page.goto('http://localhost:5173/')
     await page.getByRole('button', { name: 'login' }).click()
     await page.locator('input[type="text"]').click()
     await page.locator('input[type="text"]').fill('')
