69c23ecb71a8e2488c55ae5d121afdd1f0826824
lemon <lemon@example.com>
2024-10-13 03:03:23 +0300

    Changes

--- server/tests/blog_api.js
+++ server/tests/blog_api.js
@@ -369,107 +369,107 @@ describe('Backend testing', () => {
     })
   })
 
-  // describe('when there is initially one user in db', () => {
-  //   beforeEach(async () => {
-  //     await User.deleteMany({})
+  describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+      await User.deleteMany({})
 
-  //     const passwordHash = await bcrypt.hash('sekret', 10)
-  //     const user = new User({ username: 'root', passwordHash })
+      const passwordHash = await bcrypt.hash('sekret', 10)
+      const user = new User({ username: 'root', passwordHash })
 
-  //     await user.save()
-  //   })
+      await user.save()
+    })
 
-  //   test('creation succeeds with a fresh username', async () => {
-  //     const usersAtStart = await helper.usersInDb()
+    test('creation succeeds with a fresh username', async () => {
+      const usersAtStart = await helper.usersInDb()
 
-  //     const newUser = {
-  //       username: 'mluukkai',
-  //       name: 'Matti Luukkainen',
-  //       password: 'salainen'
-  //     }
+      const newUser = {
+        username: 'mluukkai',
+        name: 'Matti Luukkainen',
+        password: 'salainen'
+      }
 
-  //     await api
-  //       .post('/api/users')
-  //       .send(newUser)
-  //       .expect(201)
-  //       .expect('Content-Type', /application\/json/)
+      await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  //     const usersAtEnd = await helper.usersInDb()
-  //     assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+      const usersAtEnd = await helper.usersInDb()
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
 
-  //     const usernames = usersAtEnd.map((u) => u.username)
-  //     assert(usernames.includes(newUser.username))
-  //   })
+      const usernames = usersAtEnd.map((u) => u.username)
+      assert(usernames.includes(newUser.username))
+    })
 
-  //   test('creation fails with proper statuscode and message if username already taken', async () => {
-  //     const usersAtStart = await helper.usersInDb()
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+      const usersAtStart = await helper.usersInDb()
 
-  //     const newUser = {
-  //       username: 'root',
-  //       name: 'Superuser',
-  //       password: 'salainen'
-  //     }
+      const newUser = {
+        username: 'root',
+        name: 'Superuser',
+        password: 'salainen'
+      }
 
-  //     const result = await api
-  //       .post('/api/users')
-  //       .send(newUser)
-  //       .expect(400)
-  //       .expect('Content-Type', /application\/json/)
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
 
-  //     const usersAtEnd = await helper.usersInDb()
-  //     assert(result.body.error.includes('expected `username` to be unique'))
+      const usersAtEnd = await helper.usersInDb()
+      assert(result.body.error.includes('expected `username` to be unique'))
 
-  //     assert.strictEqual(usersAtEnd.length, usersAtStart.length)
-  //   })
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
 
-  //   test('creation fails with proper statuscode and message if username is too short', async () => {
-  //     const usersAtStart = await helper.usersInDb()
+    test('creation fails with proper statuscode and message if username is too short', async () => {
+      const usersAtStart = await helper.usersInDb()
 
-  //     const newUser = {
-  //       username: 'qw',
-  //       name: 'Shortname',
-  //       password: 'normpass'
-  //     }
+      const newUser = {
+        username: 'qw',
+        name: 'Shortname',
+        password: 'normpass'
+      }
 
-  //     const result = await api
-  //       .post('/api/users')
-  //       .send(newUser)
-  //       .expect(400)
-  //       .expect('Content-Type', /application\/json/)
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
 
-  //     const usersAtEnd = await helper.usersInDb()
-  //     assert(
-  //       result.body.error.includes(
-  //         'username must be at least 3 characters long'
-  //       )
-  //     )
-  //     assert.strictEqual(usersAtEnd.length, usersAtStart.length)
-  //   })
+      const usersAtEnd = await helper.usersInDb()
+      assert(
+        result.body.error.includes(
+          'username must be at least 3 characters long'
+        )
+      )
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
 
-  //   test('creation fails with proper statuscode and message if password is too short', async () => {
-  //     const usersAtStart = await helper.usersInDb()
+    test('creation fails with proper statuscode and message if password is too short', async () => {
+      const usersAtStart = await helper.usersInDb()
 
-  //     const newUser = {
-  //       username: 'qwerty',
-  //       name: 'Normname',
-  //       password: 'qw'
-  //     }
+      const newUser = {
+        username: 'qwerty',
+        name: 'Normname',
+        password: 'qw'
+      }
 
-  //     const result = await api
-  //       .post('/api/users')
-  //       .send(newUser)
-  //       .expect(400)
-  //       .expect('Content-Type', /application\/json/)
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
 
-  //     const usersAtEnd = await helper.usersInDb()
-  //     assert(
-  //       result.body.error.includes(
-  //         'password must be at least 3 characters long'
-  //       )
-  //     )
-  //     assert.strictEqual(usersAtEnd.length, usersAtStart.length)
-  //   })
-  // })
+      const usersAtEnd = await helper.usersInDb()
+      assert(
+        result.body.error.includes(
+          'password must be at least 3 characters long'
+        )
+      )
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
+  })
 
   // // Always last test to close the database connection
   // test('zz_close database connection', async () => {
--- server/tests/blog_api_temp.test.js
+++ server/tests/blog_api_temp.test.js
@@ -388,27 +388,129 @@ describe('Backend testing', () => {
     })
   })
 
+  describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+      await User.deleteMany({})
+
+      const passwordHash = await bcrypt.hash('sekret', 10)
+      const user = new User({ username: 'root', passwordHash })
+
+      await user.save()
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: 'mluukkai',
+        name: 'Matti Luukkainen',
+        password: 'salainen'
+      }
+
+      await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const usersAtEnd = await helper.usersInDb()
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+      const usernames = usersAtEnd.map((u) => u.username)
+      assert(usernames.includes(newUser.username))
+    })
+
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: 'root',
+        name: 'Superuser',
+        password: 'salainen'
+      }
+
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      const usersAtEnd = await helper.usersInDb()
+      assert(result.body.error.includes('expected `username` to be unique'))
+
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
+
+    test('creation fails with proper statuscode and message if username is too short', async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: 'qw',
+        name: 'Shortname',
+        password: 'normpass'
+      }
+
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      const usersAtEnd = await helper.usersInDb()
+      assert(
+        result.body.error.includes(
+          'username must be at least 3 characters long'
+        )
+      )
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
+
+    test('creation fails with proper statuscode and message if password is too short', async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: 'qwerty',
+        name: 'Normname',
+        password: 'qw'
+      }
+
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      const usersAtEnd = await helper.usersInDb()
+      assert(
+        result.body.error.includes(
+          'password must be at least 3 characters long'
+        )
+      )
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
+  })
+
   // Always last test to close the database connection
-  // test('zz_close database connection', async () => {
-  //   console.log('Closing database connection...')
-  //   await User.deleteMany({})
-  //   console.log('users deleted.')
-  //   await mongoose.connection.close()
-  //   console.log('Database connection closed.')
-  // })
-
-})
-
-after(async () => {
-  console.log('Running after hook...')
-  await User.deleteMany({})
-  if (mongoose.connection.readyState !== 0) {
+  test('zz_close database connection', async () => {
+    console.log('Closing database connection...')
+    await User.deleteMany({})
+    console.log('users deleted.')
     await mongoose.connection.close()
     console.log('Database connection closed.')
-  } else {
-    console.log('Database connection was already closed.')
-  }
+  })
+
 })
+
+// after(async () => {
+//   console.log('Running after hook...')
+//   await User.deleteMany({})
+//   if (mongoose.connection.readyState !== 0) {
+//     await mongoose.connection.close()
+//     console.log('Database connection closed.')
+//   } else {
+//     console.log('Database connection was already closed.')
+//   }
+// })
 
 })
 // after(async () => {
